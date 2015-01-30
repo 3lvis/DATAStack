0f4f3cf881db41b54edd5269a32c3985a85220c8
Elvis Nuñez <elvisnunez@me.com>
2015-01-30 15:03:30 +0100

    Revert "Improve addSkipBackupAttributeToItemAtURL"
    This reverts commit 4bb8f245eba3c8a27e7e731f53b7994c30410c40.

--- ANDYDataManager/ANDYDataManager.m
+++ ANDYDataManager/ANDYDataManager.m
@@ -126,14 +126,7 @@
 #if !TARGET_IPHONE_SIMULATOR
 - (BOOL)addSkipBackupAttributeToItemAtURL:(NSURL *)URL
 {
-    assert([[NSFileManager defaultManager] fileExistsAtPath:URL.path]);
-
-    NSError *error = nil;
-    BOOL success = [URL setResourceValue:[NSNumber numberWithBool:YES]
-                                  forKey:NSURLIsExcludedFromBackupKey error:&error];
-    if (!success)NSLog(@"Error excluding %@ from backup %@", [URL lastPathComponent], error);
-
-    return success;
+    return [URL setResourceValue:[NSNumber numberWithBool:YES] forKey:NSURLIsExcludedFromBackupKey error:nil];
 }
 #endif
 
