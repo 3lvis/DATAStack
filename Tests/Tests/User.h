e3a142b2dceb6afccf9c671e4918d6946bbd198f
Elvis Nuñez <elvisnunez@me.com>
2015-02-08 20:27:57 +0100

    Clean up test generated models

--- Tests/Tests/User.h
+++ Tests/Tests/User.h
@@ -1,14 +1,5 @@
-//
-//  User.h
-//  Tests
-//
-//  Created by Elvis Nuñez on 2/7/15.
-//  Copyright (c) 2015 ORGANIZATION. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
-
+@import Foundation;
+@import CoreData;
 
 @interface User : NSManagedObject
 
