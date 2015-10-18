f9e33949ee9a6c55d6d1dab905671581280daa3c
Elvis Nuñez <elvisnunez@me.com>
2015-10-19 00:51:04 +0200

    Add Swift Demo

---
+++ Pod/DemoSwift/ViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+import CoreData
+import DATASource
+
+class ViewController: UITableViewController {
+
+    var dataStack: DATAStack?
+
+    lazy var dataSource: DATASource = {
+        let request: NSFetchRequest = NSFetchRequest(entityName: "User")
+        request.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+
+        let dataSource = DATASource(tableView: self.tableView, cellIdentifier: "Cell", fetchRequest: request, mainContext: self.dataStack!.mainContext, configuration: { cell, item, indexPath in
+            cell.textLabel?.text = item.valueForKey("name") as? String
+        })
+
+        return dataSource
+    }()
+
+    convenience init(dataStack: DATAStack) {
+        self.init(style: .Plain)
+
+        self.dataStack = dataStack
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: "Cell")
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "saveAction")
+        self.tableView.dataSource = self.dataSource
+    }
+
+    func saveAction() {
+        self.dataStack!.performInNewBackgroundContext { backgroundContext in
+            if let entity = NSEntityDescription.entityForName("User", inManagedObjectContext: backgroundContext) {
+                let user = NSManagedObject(entity: entity, insertIntoManagedObjectContext: backgroundContext)
+
+                let name = self.randomString()
+                user.setValue(name, forKey: "name")
+
+                do {
+                    try backgroundContext.save()
+                } catch let savingError as NSError {
+                    print("Could not save \(savingError)")
+                } catch {
+                    fatalError()
+                }
+
+                self.dataStack!.persistWithCompletion({ })
+            } else {
+                print("Oh no")
+            }
+        }
+    }
+
+    func randomString() -> String {
+        let letters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÅØÆ"
+        var string = ""
+        for _ in 0...10 {
+            let token = UInt32(letters.characters.count)
+            let letterIndex = Int(arc4random_uniform(token))
+            let firstChar = Array(letters.characters)[letterIndex]
+            string.append(firstChar)
+        }
+
+        return string
+    }
+}
