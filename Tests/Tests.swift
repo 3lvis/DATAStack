0b3294a0a6fdd72a9ff62edbec434670d5639491
Elvis Nuñez <elvisnunez@me.com>
2016-10-09 15:17:16 +0200

    Improve comment
    [skip ci]

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -150,7 +150,7 @@ class Tests: XCTestCase {
         let objects = self.fetch(in: firstDataStack.mainContext)
         XCTAssertEqual(objects.count, 1)
 
-        // ChangedDataModel adds the updatedDate attribute to the model
+        // ChangedDataModel is a copy of DataModel with the main difference that adds the updatedDate attribute.
         let secondDataStack = DATAStack(modelName: "ChangedDataModel", bundle: Bundle(for: Tests.self), storeType: .sqLite, storeName: "Shared")
         let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "User")
         fetchRequest.predicate = NSPredicate(format: "remoteID = %@", NSNumber(value: 1))
